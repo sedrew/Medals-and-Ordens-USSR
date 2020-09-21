28fb738467c279d0e96bb633472f0b77de62f286
Aidar <aydar.ibradimov.96@inbox.ru>
2020-09-21 20:06:02 +0400

    Languages

--- resource/languages.lua
+++ resource/languages.lua
@@ -1,11 +1,11 @@
 return {
   en = {
-    name_game = "Медали и Ордена",
-    play = "Играть",
-    home = "Домой",
-    back = "Назад",
-    restart = "Заново",
-    setting = "Настройки",
+    name_game = "Medals and Orders",
+    play = "Play",
+    home = "Home",
+    back = "Back",
+    restart = "Restart",
+    setting = "Settings",
     age_msg = "выше %{age}.",
     phone_msg = {
       one = "you have one new message.",
@@ -13,12 +13,12 @@ return {
     }
   },
   ru = {
-    name_game = "Medals and Orders",
-    play = "Play",
-    home = "Home",
-    back = "Back",
-    restart = "Restart",
-    setting = "Settings",
+    name_game = "Медали и Ордена",
+    play = "Играть",
+    home = "Домой",
+    back = "Назад",
+    restart = "Заново",
+    setting = "Настройки",
     age_msg = "выше %{age}.",
     phone_msg = {
       one = "you have one new message.",
